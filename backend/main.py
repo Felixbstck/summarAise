16e3c8fe34a87c0ae0dd5cbf86f20377a65c4888
Felix Bostock <felixbostock@Felixs-MacBook-Air.local>
2026-07-29 19:03:13 +0100

    Added upload post request

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from pypdf import PdfReader
+import io
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+    contents = await file.read()
+    reader = PdfReader(io.BytesIO(contents))
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text() or ""
+    return {
+        "filename": file.filename,
+        "num_pages": len(reader.pages),
+        "preview": text,
+    }
